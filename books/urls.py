9945bc0e837ed933949eb3c608053885aeec4d6c
Dhruvsurani <dhruvsurani2000@gmail.com>
2022-09-27 19:08:20 +0530

    new one

--- books/urls.py
+++ books/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BookCreateView, BookListView, BookDetailView
 
 urlpatterns = [
-    path('book/create/', BookCreateView.as_view(), name='book_create'),
     path('', BookListView.as_view(), name='book_list'),
-    path('book/<int:pk>', BookDetailView.as_view(), name='book-detail'),
+    path('book/create/', BookCreateView.as_view(), name='book_create'),
+    path('book/<slug:slug>', BookDetailView.as_view(), name='book-detail'),
 ]
