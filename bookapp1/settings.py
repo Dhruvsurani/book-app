df108cd8d9faf616c33f0c4d71d4440a15ad5492
Dhruvsurani <dhruvsurani2000@gmail.com>
2022-09-29 18:30:56 +0530

    new one

--- bookapp1/settings.py
+++ bookapp1/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'users',
     'books',
+    'django_crontab',
 ]
 
 MIDDLEWARE = [
--- users/forms.py
+++ users/forms.py
@@ -2,11 +2,6 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
-my_choices = (
-    ('1', "Admin"),
-    ('2', "Buyer")
-)
-
 
 class SignUp(UserCreationForm):
     username = forms.CharField(max_length=40)
